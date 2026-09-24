621ac88737ac8ce8ae5836f0f2fcb54275b87dfc
ykolacze <yohan.kolaczek@gmail.com>
2026-09-24 18:31:14 +0200

    feat(backend): configure daphne asgi server for production and websocket support

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+ASGI_APPLICATION = 'config.asgi.application'
 
 
 # Recuperation des variables d'env
