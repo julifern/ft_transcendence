ecaa5138c1e35a39d604be558fb1fcf97443431e
ykolacze <yohan.kolaczek@gmail.com>
2026-09-24 18:05:19 +0200

    feat(db): switch from sqlite to postgresql with persistent volume

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,12 +89,16 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 # Database
-# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/stable/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'transcendence_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'transcendence_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
