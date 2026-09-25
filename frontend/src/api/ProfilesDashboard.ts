34cdb9fe0576b03c59b2e96cf4894920c9b161a3
lasere77 <colin.mewen@gmail.com>
2026-09-25 13:26:13 +0200

    fix(fetch url): remove hardcoded localhost base URL from fetch requests, all requests pass through nginx now.

--- frontend/src/api/ProfilesDashboard.ts
+++ frontend/src/api/ProfilesDashboard.ts
@@ -3,7 +3,7 @@ import type { ProfilesDashboard } from '../types/ObjStudent';
 
 export async function getProfilesDashboard(): Promise<ProfilesDashboard> {
   const res = await fetch(
-    "http://localhost:8000/auth/api/dashboard/",
+    "auth/api/dashboard/",
     {
       credentials: "include",
     }
